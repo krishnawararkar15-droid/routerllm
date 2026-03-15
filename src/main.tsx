b81f0943237cd060929bba4f0bd723705b933165
Your Name <you@example.com>
2026-03-15 12:10:01 +0530

    add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { initPaddle } from './lib/paddle';
+import { inject } from '@vercel/analytics';
 
+inject();
 initPaddle();
 
 createRoot(document.getElementById('root')!).render(
